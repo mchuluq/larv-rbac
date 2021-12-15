2153f1e31446c489d118c4b695a7d15483331fa1
mochammad chusnul chuluq <mchuluq@gmail.com>
2021-12-16 03:06:22 +0700

    fix sensitive case

--- src/Routes.php
+++ src/Routes.php
@@ -19,7 +19,7 @@ Route::group(['middleware' => 'web'], function () {
     // webauthn
     Route::post('webauthn/login/details','Mchuluq\Larv\Rbac\Http\Controllers\WebauthnController@loginDetails')->name('webauthn.login.details');
     Route::post('webauthn/login', 'Mchuluq\Larv\Rbac\Http\Controllers\WebauthnController@login')->name('webauthn.login');
-    Route::post('webauthn/create/details', 'Mchuluq\Larv\Rbac\Http\Controllers\WebAuthnController@createDetails')->middleware('rbac-auth')->name('webauthn.create.details');
-    Route::post('webauthn/create', 'Mchuluq\Larv\Rbac\Http\Controllers\WebAuthnController@create')->middleware('rbac-auth')->name('webauthn.create');
-    Route::match(['get','delete'],'webauthn/user', 'Mchuluq\Larv\Rbac\Http\Controllers\WebAuthnController@user')->middleware('rbac-auth')->name('webauthn.user');
+    Route::post('webauthn/create/details', 'Mchuluq\Larv\Rbac\Http\Controllers\WebauthnController@createDetails')->middleware('rbac-auth')->name('webauthn.create.details');
+    Route::post('webauthn/create', 'Mchuluq\Larv\Rbac\Http\Controllers\WebauthnController@create')->middleware('rbac-auth')->name('webauthn.create');
+    Route::match(['get','delete'],'webauthn/user', 'Mchuluq\Larv\Rbac\Http\Controllers\WebauthnController@user')->middleware('rbac-auth')->name('webauthn.user');
 });
